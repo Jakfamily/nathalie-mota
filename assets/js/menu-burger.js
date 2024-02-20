6f7611f2c614fa027b1224cb6f13050dd3c1bd1c
jakfamily <fariajeanbap@gmail.com>
2024-02-20 15:01:35 +0100

    retravaille de la logique du menu burger lors de louverture

--- assets/js/menu-burger.js
+++ assets/js/menu-burger.js
@@ -17,5 +17,23 @@ $(function () {
     header.toggleClass("open", !isOpen);
     menuBurger.toggleClass("open", !isOpen);
     nav.toggleClass("open", !isOpen);
+
+    // Bloque ou réactive le défilement du corps en fonction de l'état du menu
+    if (!isOpen) {
+      $("body").css("overflow", "hidden");
+    } else {
+      $("body").css("overflow", "auto");
+    }
+  });
+
+  // Ajoute un gestionnaire d'événement pour la fermeture du menu au clic sur un lien
+  menuLinks.on("click", function () {
+    // Ferme le menu
+    header.removeClass("open");
+    menuBurger.removeClass("open");
+    nav.removeClass("open");
+
+    // Réactive le défilement du corps
+    $("body").css("overflow", "auto");
   });
 });
