9b41200ec524e05b0476975ebec0e702383b946e
jakfamily <fariajeanbap@gmail.com>
2024-02-23 10:31:58 +0100

    remise a zero de la partie filtre

--- front-page.php
+++ front-page.php
@@ -1,7 +1,6 @@
 <?php get_header(); ?>
 <?php get_template_part('template-parts/banner'); ?>
 
-<?php get_template_part('template-parts/filtre'); ?>
 
 <section id="containerPhoto" class="blocCatalogue">
   <?php get_template_part('template-parts/section-photo'); ?>
